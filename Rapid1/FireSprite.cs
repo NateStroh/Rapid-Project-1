12c778835be08d2be402c4747fdfc6037a8ca897
Nate Strohmyer <nate.stroh@live.com>
2020-09-07 22:39:16 -0600

    fixed fire trail scaling

--- Rapid1/FireSprite.cs
+++ Rapid1/FireSprite.cs
@@ -34,25 +34,25 @@ class FireSprite : Sprite
     {
         if (p.SpeedY > 3000 || p.SpeedX > 3600)
         {
-            animationManager.scale = 1.5f;
+            animationManager.a.scale = 1.5f;
             animationManager.animationPosition += new Vector2(-100, -100);
             animationManager.playAnimation(animations["Rfire"]);
         }
         else if (p.SpeedY > 1000 || p.SpeedX > 3000)
         {
-            animationManager.scale = 1.3f;
+            animationManager.a.scale = 1.3f;
             animationManager.animationPosition += new Vector2(-50, -50);
             animationManager.playAnimation(animations["Yfire"]);
         }
         else if (p.SpeedY > 500 || p.SpeedX > 500)
         {
-            animationManager.scale = 1.1f;
+            animationManager.a.scale = 1.1f;
             animationManager.animationPosition += new Vector2(-50, -50);
             animationManager.playAnimation(animations["Gfire"]);
         }
         else
         {
-            animationManager.scale = .5f;
+            animationManager.a.scale = .5f;
             animationManager.animationPosition += new Vector2(100, 100);
         }
     }
